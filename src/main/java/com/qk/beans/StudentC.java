3753bb167ee142f5447665011e80fe5dcb1cd227
qiankai <824025757@qq.com>
2020-10-28 09:00:54 +0800

    修复

--- src/main/java/com/qk/beans/StudentC.java
+++ src/main/java/com/qk/beans/StudentC.java
@@ -2,7 +2,7 @@ package com.qk.beans;
 
 public class StudentC {
     private StudentA studentA ;
-    private double pi=3.14;
+    private String t="abc";
 
     public void setStudentA(StudentA studentA) {
         this.studentA = studentA;
