bdbda3079326e31ffe4b1325bc1ba6d81a2708b7
824025757 <824025757@qq.com>
2020-10-28 09:36:12 +0800

    huxuan

--- src/main/java/com/qk/beans/StudentC.java
+++ src/main/java/com/qk/beans/StudentC.java
@@ -2,7 +2,7 @@ package com.qk.beans;
 
 public class StudentC {
     private StudentA studentA ;
-    private String qk="qiankai";
+    private String hx="huxuan";
 
     public void setStudentA(StudentA studentA) {
         this.studentA = studentA;
