871022a3190c58b478cb3265b9959a1823c1a9a2
824025757 <824025757@qq.com>
2020-10-28 08:24:21 +0800

    增加了一个字段

--- src/main/java/com/qk/beans/StudentC.java
+++ src/main/java/com/qk/beans/StudentC.java
@@ -2,6 +2,7 @@ package com.qk.beans;
 
 public class StudentC {
     private StudentA studentA ;
+    private double pi=3.14;
 
     public void setStudentA(StudentA studentA) {
         this.studentA = studentA;
