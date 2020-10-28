7168c15ecc52a342e566697bfd336a394bd50b61
qiankai <824025757@qq.com>
2020-10-28 08:59:29 +0800

    还原

--- src/main/java/com/qk/beans/StudentC.java
+++ src/main/java/com/qk/beans/StudentC.java
@@ -2,7 +2,6 @@ package com.qk.beans;
 
 public class StudentC {
     private StudentA studentA ;
-    private String tt="test";
 
     public void setStudentA(StudentA studentA) {
         this.studentA = studentA;
