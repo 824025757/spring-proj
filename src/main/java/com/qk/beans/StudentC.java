19465ba92f80430fe4ac32ad41e8392fa52eac5f
qiankai <824025757@qq.com>
2020-10-28 08:27:45 +0800

    在idea中测试git

--- src/main/java/com/qk/beans/StudentC.java
+++ src/main/java/com/qk/beans/StudentC.java
@@ -2,6 +2,7 @@ package com.qk.beans;
 
 public class StudentC {
     private StudentA studentA ;
+    private String tt="test";
 
     public void setStudentA(StudentA studentA) {
         this.studentA = studentA;
